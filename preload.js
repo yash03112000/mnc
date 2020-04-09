acc42bbab3357e42231eba4b15eddcd3d8a9ee54
yash03112000 <61574898+yash03112000@users.noreply.github.com>
2020-04-09 16:21:26 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -6,7 +6,7 @@
   function frame(){
     if(loading==100){
       clearInterval(id);
-      window.open("index.html","_self");
+      window.open("index2.html","_self");
     } else {
         loading = loading+1;
         if(loading == 90) {
