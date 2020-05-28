8e3dcf4d48595318b39d1b651c79c3c7a943714b
yash03112000 <61574898+yash03112000@users.noreply.github.com>
2020-05-28 20:57:25 +0530

    homepage mobile 2

--- public/trial6/js/new.js
+++ public/trial6/js/new.js
@@ -1,5 +1,20 @@
 
 function eventfun(){
+  var x = window.matchMedia("(max-width: 500px)")
+  if(x.matches){
+    var a=3;
+    console.log('mobile');
+    var tl = gsap.timeline();
+    tl.set(`.part${a}3`, { transformOrigin: "bottom"});
+    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+    tl.from(`.part${a}3`,{duration: .5,y:'100%'},"-=0.31")
+    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+    .call(event1,[a]);
+    // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }else{
     var a=3;
     console.log('aaa');
     var tl = gsap.timeline();
@@ -12,6 +27,8 @@ function eventfun(){
     tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
     .call(event1,[a]);
     // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }
+
 }
 function event1(a){
     window.location = "./events.html"
@@ -19,55 +36,72 @@ function event1(a){
 }
 
 function club(){
-  var a=4;
-  console.log('aaa');
-  var tl = gsap.timeline();
-  tl.set(`.part${a}3`, { transformOrigin: "left"});
-  tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
-  tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
-  tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
-  tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
-  tl.to(`.img${a}`, { duration: .3,opacity: '0' });
-  tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
-  .call(club1,[a]);
-  // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  var x = window.matchMedia("(max-width: 500px)")
+  if(x.matches){
+    var a=4;
+    console.log('aaa');
+    var tl = gsap.timeline();
+    tl.set(`.part${a}3`, { transformOrigin: "bottom"});
+    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+    tl.from(`.part${a}3`,{duration: .5,y:'100%'},"-=0.31")
+    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+    .call(club1,[a]);
+    // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }else{
+    var a=4;
+    console.log('aaa');
+    var tl = gsap.timeline();
+    tl.set(`.part${a}3`, { transformOrigin: "left"});
+    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+    tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
+    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+    .call(club1,[a]);
+    // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }
+
 }
 function club1(a){
   window.location = "./club.html"
 }
 
-function eventfun(){
-  var a=3;
-  console.log('aaa');
-  var tl = gsap.timeline();
-  tl.set(`.part${a}3`, { transformOrigin: "left"});
-  tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
-  tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
-  tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
-  tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
-  tl.to(`.img${a}`, { duration: .3,opacity: '0' });
-  tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
-  .call(event1,[a]);
-  // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
-}
-function event1(a){
-  window.location = "./events.html"
-  
-}
+
 
 function page(){
-var a=2;
-console.log('aaa');
-var tl = gsap.timeline();
-tl.set(`.part${a}3`, { transformOrigin: "left"});
-tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
-tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
-tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
-tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
-tl.to(`.img${a}`, { duration: .3,opacity: '0' });
-tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
-.call(page1,[a]);
-// tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  var x = window.matchMedia("(max-width: 500px)")
+  if(x.matches){
+    var a=2;
+    console.log('aaa');
+    var tl = gsap.timeline();
+    tl.set(`.part${a}3`, { transformOrigin: "bottom"});
+    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+    tl.from(`.part${a}3`,{duration: .5,y:'100%'},"-=0.31")
+    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+    .call(page1,[a]);
+    // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }else{
+    var a=2;
+    console.log('aaa');
+    var tl = gsap.timeline();
+    tl.set(`.part${a}3`, { transformOrigin: "left"});
+    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+    tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
+    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+    .call(page1,[a]);
+    // tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+  }
+
 }
 function page1(a){
 window.location = "./about.html"
