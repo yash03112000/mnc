1335589279a2ad4bf34ef7def6f37f958e63c87c
yash03112000 <61574898+yash03112000@users.noreply.github.com>
2020-06-13 22:12:45 +0530

    this time

--- public/trial6/js/main.js
+++ public/trial6/js/main.js
@@ -55,23 +55,47 @@ var current = 1;
                 tl.to(`.sidenav`, {duration:.2, opacity:'1' });
                 tl.set(`.part${a}3`,{y:'0%'})
             }else{
-                var tl = gsap.timeline();
-                tl.set(`.part${a}3`, { transformOrigin: "left"});
-                tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
-                tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
-                tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
-                tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
-                tl.to(`.img${a}`, { duration: .3,opacity: '0' });
-                tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
-                tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
-                tl.to(`.part${a}3`, { duration: .01,display:'none' });
-                tl.to(`.sec${b}`, { duration: .01,display: 'flex' },"-=0.2");
-                tl.from(`.sec${b}`, { duration: .4,x:'100%' },"-=0.21");
-                tl.to(`.part${b}1`, { duration: .5,y:'30px',opacity:'1' });
-                tl.to(`.img${b}`, { duration: .3,opacity: '1' })
-                    .call(updatedown,[a,b]);
-                tl.to(`.sidenav`, {duration:.2, opacity:'1' });
-                tl.set(`.part${a}3`,{x:'0%'})
+                if(a===1){
+                    var tl = gsap.timeline();
+                    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+                    tl.to(`.head1`, {duration:0.2, opacity:'0' });
+                    tl.to(`.img1`, {duration:0.2, opacity:'0' });
+                    tl.set(`.part${a}3`, { display: 'initial' });
+                    tl.from(`.part${a}3`,{duration: .5,x:'100%'})
+                    // tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+                    // tl.set(`.img${a}`, {scale: '0.7',x:"0px" })
+                    tl.set(`.sec${a}`,{duration: .01,display:'none'})
+                    tl.to(`.part${a}3`,{duration: .5,x:'-100%'})
+                    tl.to(`.part${a}3`, { duration: .01,display:'none' });
+                    tl.to(`.sec${b}`, { duration: .01,display: 'flex' },"-=0.2");
+                    tl.from(`.sec${b}`, { duration: .4,x:'100%' },"-=0.21");
+                    tl.to(`.img${b}`, { duration: .3,opacity: '1' })
+                        .call(updatedown,[a,b]);
+                    tl.to(`.img${b}`, { duration: .3,scale: '0.6',x:"40px" })
+                    tl.to(`.part${b}1`, { duration: .5,y:'30px',opacity:'1' });
+                    tl.to(`.sidenav`, {duration:.2, opacity:'1' });
+                    tl.set(`.part${a}3`,{x:'0%'})
+                }else{
+                    var tl = gsap.timeline();
+                    tl.set(`.part${a}3`, { transformOrigin: "left"});
+                    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+                    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+                    tl.to(`.part${a}3`, { duration: .01,display: 'initial' },"-=0.3");
+                    tl.from(`.part${a}3`,{duration: .5,x:'100%'},"-=0.31")
+                    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+                    tl.set(`.img${a}`, {scale: '0.7',x:"0px" })
+                    tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
+                    tl.to(`.part${a}3`,{duration: .5,x:'-100%'},"-=0.2")
+                    tl.to(`.part${a}3`, { duration: .01,display:'none' });
+                    tl.to(`.sec${b}`, { duration: .01,display: 'flex' },"-=0.2");
+                    tl.from(`.sec${b}`, { duration: .4,x:'100%' },"-=0.21");
+                    tl.to(`.img${b}`, { duration: .3,opacity: '1' })
+                        .call(updatedown,[a,b]);
+                    tl.to(`.img${b}`, { duration: .3,scale: '0.6',x:"40px" })
+                    tl.to(`.part${b}1`, { duration: .5,y:'30px',opacity:'1' });
+                    tl.to(`.sidenav`, {duration:.2, opacity:'1' });
+                    tl.set(`.part${a}3`,{x:'0%'})
+                }
             }
 
         }
@@ -97,24 +121,57 @@ var current = 1;
                 tl.to(`.sidenav`, {duration:.2, opacity:'1' });
                 tl.set(`.part03`,{y:'00%'})
             }else{
-                var tl = gsap.timeline();
-                console.log(b);
-                // tl.set(`.part${a}3`, { transformOrigin: "left"});
-                tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
-                tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
-                tl.to(`.part03`, { duration: .01,display: 'initial' },"-=0.3");
-                tl.from(`.part03`,{duration: .5,x:'-100%'},"-=0.31")
-                tl.to(`.img${a}`, { duration: .3,opacity: '0' });
-                tl.to(`.sec${a}`,{duration: .01,display:'none'},"-=0.2")
-                tl.to(`.part03`,{duration: .5,x:'100%'},"-=0.2")
-                tl.to(`.part03`, { duration: .01,display:'none' },"+=0.02");
-                tl.to(`.sec${b}`, { duration: .01,display: 'flex' },"-=0.2");
-                tl.from(`.sec${b}`, { duration: 1,x:'-100%' },"-=0.21");
-                tl.to(`.part${b}1`, { duration: .5,y:'30px',opacity:'1' })
-                tl.to(`.img${b}`, { duration: .3,opacity: '1' })
-                    .call(updateup,[a,b]);
-                tl.to(`.sidenav`, {duration:.2, opacity:'1' });
-                tl.set(`.part03`,{x:'00%'})
+                if(b===1){
+                    var tl = gsap.timeline();
+                    console.log(b);
+                    // tl.set(`.part${a}3`, { transformOrigin: "left"});
+                    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+                    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+                    tl.to(`.part03`, { duration: .01,display: 'initial' },"-=0.3");
+                    tl.from(`.part03`,{duration: .5,x:'-100%'},"-=0.31")
+                    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+                    tl.set(`.img${a}`, {scale: '0.7',x:"0px" })
+                    tl.to(`.sec${a}`,{duration: .01,display:'none'})
+                    tl.to(`.part03`,{duration: .5,x:'100%'})
+                    tl.set(`.part03`, { display:'none' });
+                    tl.set(`.part${b}3`, { display: 'initial' },"-=0.5");
+                    tl.from(`.part${b}3`,{duration: .5,x:'-100%'},"-=0.5")
+                    tl.to(`.part${b}3`,{duration: .5,x:'100%'})
+                    tl.set(`.part${b}3`, { display:'none' });
+                    tl.set(`.sec${b}`, {display: 'flex' },"-=0.5");
+                    tl.to(`.sidenav`, {duration:.2, opacity:'1' })
+                        .call(updateup,[a,b]);
+                    tl.to(`.head1`, {duration:0.2, opacity:'1' });
+                    tl.to(`.img${b}`, {duration:1,opacity: '1' })
+                    tl.set(`.part03`,{x:'00%'})
+                    tl.set(`.part${b}3`,{x:'00%'})
+                }else{
+                    var tl = gsap.timeline();
+                    console.log(b);
+                    // tl.set(`.part${a}3`, { transformOrigin: "left"});
+                    tl.to(`.sidenav`, {duration:0.2, opacity:'0' });
+                    tl.to(`.part${a}1`, { duration: .5,y:'-30px',opacity:'0' });
+                    tl.to(`.part03`, { duration: .01,display: 'initial' },"-=0.3");
+                    tl.from(`.part03`,{duration: .5,x:'-100%'},"-=0.31")
+                    tl.to(`.img${a}`, { duration: .3,opacity: '0' });
+                    tl.set(`.img${a}`, {scale: '0.7',x:"0px" })
+                    tl.to(`.sec${a}`,{duration: .01,display:'none'})
+                    tl.to(`.part03`,{duration: .5,x:'100%'})
+                    tl.set(`.part03`, { display:'none' });
+                    tl.set(`.part${b}3`, { display: 'initial' },"-=0.5");
+                    tl.from(`.part${b}3`,{duration: .5,x:'-100%'},"-=0.5")
+                    tl.to(`.part${b}3`,{duration: .5,x:'100%'})
+                    tl.set(`.part${b}3`, { display:'none' });
+                    tl.set(`.sec${b}`, {display: 'flex' },"-=0.5");
+                    tl.from(`.sec${b}`, { duration: .5,x:'-100%' },"-=0.5");
+                    tl.set(`.img${b}`, {opacity: '1' })
+                        .call(updateup,[a,b]);
+                    tl.to(`.img${b}`, { duration: .3,scale: '0.6',x:"40px" })
+                    tl.to(`.part${b}1`, { duration: .5,y:'30px',opacity:'1' })
+                    tl.to(`.sidenav`, {duration:.2, opacity:'1' });
+                    tl.set(`.part03`,{x:'00%'})
+                    tl.set(`.part${b}3`,{x:'00%'})
+                }
             }
 
         }
@@ -123,9 +180,9 @@ var current = 1;
             console.log('abcd');
             document.querySelector('.sec1').addEventListener('wheel', findScrollDirectionOtherBrowsers);
             var tl = gsap.timeline();
-            tl.from('.sec1',{duration: 1,x:'50px',opacity:0})
-            tl.to('.part11',{duration: 1,opacity:1})
-            tl.to('.img1',{duration: 1,opacity:1},"-=0.5")
+            tl.from('.sec1',{duration: 0.5,x:'50px',opacity:0})
+            tl.to('.part11',{duration: 0.5,opacity:1})
+            tl.to('.img1',{duration: 0.5,opacity:1})
             // trial();
             //barba1234();
         };
@@ -195,9 +252,17 @@ function updateup(a,b){
 
 function abc(a){
     if(current<a){
+        document.querySelector('.sec'+current).removeEventListener('wheel',findScrollDirectionOtherBrowsers);
+        setTimeout(() => {
+            document.querySelector('.sec'+(current)).addEventListener('wheel', findScrollDirectionOtherBrowsers);                        
+        }, 2000);
         scrolldown(current,a);
         current=a;
     }else if(current>a){
+        document.querySelector('.sec'+current).removeEventListener('wheel',findScrollDirectionOtherBrowsers);
+        setTimeout(() => {
+            document.querySelector('.sec'+(current)).addEventListener('wheel', findScrollDirectionOtherBrowsers);                        
+        }, 2000);
         scrollup(current,a)
         current=a;
     }
