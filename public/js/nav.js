90f413ed4b06ce99fe6b3090dd4925cfeaa8ab04
yash03112000 <yashag@iitk.ac.in>
2020-06-29 13:24:11 +0530

    deleting

--- public/js/nav.js
+++
@@ -1,9 +0,0 @@
-const toggleCollapse = document.querySelector('.toggle-collapse span');
-const nav = document.querySelector('.nav');
-
-
-// onclick event on toggle Collapse span tag
-toggleCollapse.onclick = (e) => {
-    nav.classList.toggle("collapse");
-    e.target.classList.toggle("toggle-click");
-}
