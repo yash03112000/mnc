75c5ceb17eaed3b007f5c93e67de6c582d7dd68a
yash03112000 <yashag@iitk.ac.in>
2020-07-27 22:27:45 +0530

    table

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-var main = function(){
+var main = function(res2){
     console.log('Starting Shits');
-    const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+    const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -25,24 +25,19 @@ const TOKEN_PATH = 'token.json';
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
+
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris);
+      client_id, client_secret, redirect_uris[1]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
+    if (err) return getNewToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
   });
@@ -54,7 +49,7 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -67,7 +62,7 @@ function getAccessToken(oAuth2Client, callback) {
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error('Error while trying to retrieve access token', err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
@@ -79,83 +74,36 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'en.indian#holiday@group.v.calendar.google.com',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
+function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1HPdEkTrVTp93lbUNm46koywheKArzOp9ukueW9xyuD4',
+    range: 'Sheet1!C2:H82',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
+    const rows = res.data.values;
+    if (rows.length) {
+      // console.log('Name, Major:');
+      res2.status(200).json({
+        rows:rows
+      })
+      // rows.map((row) => {
+      //   // console.log(row);
+
+      // });
     } else {
-      console.log('No upcoming events found.');
+      console.log('No data found.');
     }
   });
+
 }
 };
 
-// var authorize = function(callback){
-//     const client = new google.auth.JWT(
-//         keys.client_email,
-//         null,
-//         keys.private_key,
-//         ['https://www.googleapis.com/auth/calendar']
-//     );
-    
-//     client.authorize(function(err,tokens){
-    
-//         if(err){
-//             console.log(err);
-//             return;
-//         }else{
-//             console.log('Connected to Sheets');
-//             gsrun(client);
-//         }
-//     });
-//     console.log('Authorized');
-//     callback();
-// };
 
-// function sheets(auth) {
-//     gsrun = async function(cl){
-//         const calendar = google.calendar({version: 'v3', auth});
-//     calendar.events.list({
-//       calendarId: 'en.indian#holiday',
-//       timeMin: (new Date()).toISOString(),
-//       maxResults: 10,
-//       singleEvents: true,
-//       orderBy: 'startTime',
-//     }, (err, res) => {
-//       if (err) return console.log('The API returned an error: ' + err);
-//       const events = res.data.items;
-//       if (events.length) {
-//         console.log('Upcoming 10 events:');
-//         events.map((event, i) => {
-//           const start = event.start.dateTime || event.start.date;
-//           console.log(`${start} - ${event.summary}`);
-//         });
-//       } else {
-//         console.log('No upcoming events found.');
-//       }
-//     });
-//     }
-    
-//   }
 
-main();
+app.get('/table',(req,res)=>{
+  main(res);
+})
 
 // Start Server
 var PORT =  process.env.PORT || 3000 ;
