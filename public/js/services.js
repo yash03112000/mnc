c9f24df419e6ae6e78b456c0b096a8fe4233f4f8
yash03112000 <yashag@iitk.ac.in>
2020-07-29 18:57:29 +0530

    services

---
+++ public/js/services.js
@@ -0,0 +1,34 @@
+function openform() {
+    document.getElementById("mySidenav2").style.width = "100vw";
+    // var a = document.querySelector('main');
+    // a.addEventListener('click',closeNav)
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+    var a = document.querySelector('main');
+    a.removeEventListener('click',closeNav)
+  }
+
+
+  function openform2() {
+    document.getElementById("mySidenav3").style.width = "100vw";
+    // var a = document.querySelector('main');
+    // a.addEventListener('click',closeNav)
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav3").style.width = "0";
+    var a = document.querySelector('main');
+    a.removeEventListener('click',closeNav)
+  }
+// var equip = document.getElementById('equip')
+//  var equi.onsubmit=(e)=>{
+//   console.log('aa')
+//   e.preventDefault();
+
+//   console.log(new FormData(equip))
+// }
+
+
+
