cd1b9458f7cc7a122a4c0d5acf79a94cfe6964c8
aman7625 <aym11104017@gmail.com>
2020-07-30 19:22:28 +0530

    navbar updates

--- public/js/events.js
+++ public/js/events.js
@@ -442,6 +442,8 @@ const navbar = ()=>{
   items[1].addEventListener('mouseover',()=>{imgtrans(2)})
   items[2].addEventListener('mouseover',()=>{imgtrans(3)})
   items[3].addEventListener('mouseover',()=>{imgtrans(4)})
+  items[4].addEventListener('mouseover',()=>{imgtrans(5)})
+  items[5].addEventListener('mouseover',()=>{imgtrans(6)})
 
   // items.forEach((element,index)=>{
   //   element.addEventListener('mouseover',imgtrans)
@@ -458,6 +460,8 @@ var imgtrans = (a)=>{
   color[2] = '#e65f5c';
   color[3] = '#84dcc6';
   color[4] = '#ffff82';
+  color[5] = '#ff8c42';
+  color[6] = '#ffff82';
 
   if(a!=active && tra === 0){
     console.log(a)
